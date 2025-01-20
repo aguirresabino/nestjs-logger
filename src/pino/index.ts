2d10e84b231d192a576d1134df8af1d2368d5848
Aguirre Sabino <aguirresabino187@gmail.com>
2025-01-20 04:59:44 +0000

    chore: creates PINO_LOGGER_OPTIONS_DEFAULT const

--- src/pino/index.ts
+++ src/pino/index.ts
@@ -1,3 +1,4 @@
 export * from './create-decorated-pino-providers.helper';
+export * from './pino-logger-options-default.const';
 export * from './pino-logger.factory';
 export * from './pino.logger';
