35b90c6edfdb31181d7a9a6cbcc7072ebe24a52b
Aguirre Sabino <aguirresabino187@gmail.com>
2025-02-23 20:48:27 +0000

    feat: add enableHttpLogging option and implement HttpLoggerMiddleware

---
+++ examples/pino-log-http-requests.ts
@@ -0,0 +1,54 @@
+import { Controller, Get, Logger, Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import {
+  AppLoggerFactory,
+  DEFAULT_APP_LOGGER,
+  LoggerModule,
+  LoggerModuleOptions,
+  LoggerModuleOptionsFactory,
+} from '../src';
+
+@Controller()
+export class AppController {
+  @Get('hello')
+  getHello(): Record<string, string> {
+    return { message: 'Hello World' };
+  }
+}
+
+export class LoggerConfigService implements LoggerModuleOptionsFactory {
+  create(): LoggerModuleOptions {
+    return {
+      pino: {
+        enabled: true,
+        level: 'debug',
+      },
+      enableHttpLogging: true, // Enable HTTP logging
+    };
+  }
+}
+
+@Module({
+  imports: [
+    LoggerModule.forRootAsync({
+      useClass: LoggerConfigService,
+    }),
+  ],
+  controllers: [AppController],
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class AppModule {}
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: false,
+    logger: AppLoggerFactory.get(),
+  });
+  app.useLogger(app.get(DEFAULT_APP_LOGGER));
+  const logger = app.get<Logger>(DEFAULT_APP_LOGGER);
+  const serverPort = 3000;
+  await app.listen(serverPort);
+  logger.log('Application is running on: http://localhost:3000/hello');
+}
+void bootstrap();
